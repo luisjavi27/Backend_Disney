0d7225aa150bfaf3d236c409f4fb526398da77cb
luis.iglesias <luisjavi27@gmail.com>
2022-08-01 14:15:40 -0500

    add EER image

--- Backend_Disney/SRC/app.js
+++ Backend_Disney/SRC/app.js
@@ -25,13 +25,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-// app.use((_, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+app.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 
 app.use('/api/genres/', genreRoutes);
